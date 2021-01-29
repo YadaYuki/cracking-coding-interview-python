709a4e2a97d8ff07e521666a92b9e7812df27dea
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-01-29 22:47:29 +0900

    add delete multiple

--- 2/delete-multiple.py
+++ 2/delete-multiple.py
@@ -1,1 +1,26 @@
 from node import LinkedList
+
+l = LinkedList(head_data=1)
+
+data = [1,2,3,4,2,1,3,5,2,2,4]
+
+for item in data:
+    l.append_node(item)
+
+
+node = l.head
+
+
+print(l)
+
+hash = {}
+
+while node != None:
+    if hash.get(node.data) == True:
+        node.prev.next = node.next
+    else:
+        hash[node.data] = True
+    node = node.next
+
+print(hash)
+print(l)
--- 2/node.py
+++ 2/node.py
@@ -1,6 +1,7 @@
 class Node:
     def __init__(self,data:int):
         self.data = data
+        self.prev = None
         self.next = None
 
 
@@ -12,7 +13,14 @@ class LinkedList:
 
     def append_node(self,data:int):
         self.tail.next = Node(data)
+        self.tail.next.prev = self.tail
         self.tail = self.tail.next
 
-
-
+    def __str__(self):
+        s = ""
+        node = self.head
+        while node != None:
+            s += "{}->".format(node.data)
+            node = node.next
+            
+        return s
