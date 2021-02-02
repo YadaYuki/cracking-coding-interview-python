5b8831a6c02554e5fcb9e9297233cfbf23bcb6a6
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-02-02 22:38:07 +0900

    add Unidirectional LinkedList

--- 2/is_palindrome.py
+++ 2/is_palindrome.py
@@ -1,24 +1,12 @@
 # 2.6
 from linked_list import LinkedList,Node,initialize_linked_list_from_array
 
-def is_palindrome(a:LinkedList,b:LinkedList) -> bool:
-    if a.length != b.length:
-        return False
-    
-    na,nb = a.head,b.tail
-
-    while (na is not None) and (nb is not None):
-        if na.data != nb.data:
-            return False
-        na,nb = na.next,nb.prev
-
+def is_palindrome(l:LinkedList) -> bool:
     return True
 
 
 if __name__ == "__main__":
-
-    a,b = [7,1,6],[6,1,7]
-    
-    la,lb = initialize_linked_list_from_array(a),initialize_linked_list_from_array(b)
-    
-    print(is_palindrome(la,lb)) # True
+    l = initialize_linked_list_from_array([1,1,2,2,1,1])
+    print(is_palindrome(l)) # True
+    l = initialize_linked_list_from_array([1,1,2,1,1])
+    print(is_palindrome(l)) # True
--- 2/linked_list.py
+++ 2/linked_list.py
@@ -1,18 +1,18 @@
-class Node:
+class NodeBidirectional:
     def __init__(self,data:int):
         self.data = data
-        self.prev:Node = None
-        self.next:Node = None
+        self.prev:NodeBidirectional = None
+        self.next:NodeBidirectional = None
 
-class LinkedList: 
+class LinkedListBidirectional: 
 
     def __init__(self,head_data:int):
-        self.head = Node(head_data)
+        self.head = NodeBidirectional(head_data)
         self.tail = self.head
         self.length = 1
 
     def append_node(self,data:int):
-        self.tail.next = Node(data)
+        self.tail.next = NodeBidirectional(data)
         self.tail.next.prev = self.tail
         self.tail = self.tail.next
         self.length += 1
@@ -25,14 +25,45 @@ class LinkedList:
             node = node.next
         return s
     
-    def get_node(self,k:int) -> Node:
+    def get_node(self,k:int) -> NodeBidirectional:
         n = self.head
-        for i in range(k):
+        for _ in range(k):
             n = n.next
         return n
 
-def initialize_linked_list_from_array(array)->LinkedList:
-    l = LinkedList(head_data=array[0])
+class NodeUnidirectional:
+    def __init__(self,data:int):
+        self.data = data
+        self.next:NodeUnidirectional = None
+
+class LinkedListNodeUnidirectional: 
+
+    def __init__(self,head_data:int):
+        self.head = NodeUnidirectional(head_data)
+        self.tail = self.head
+        self.length = 1
+
+    def append_node(self,data:int):
+        self.tail.next = NodeUnidirectional(data)
+        self.tail = self.tail.next
+        self.length += 1
+
+    def __str__(self):
+        s = ""
+        node = self.head
+        while node != None:
+            s += "{}->".format(node.data)
+            node = node.next
+        return s
+    
+    def get_node(self,k:int) -> NodeUnidirectional:
+        n = self.head
+        for _ in range(k):
+            n = n.next
+        return n
+
+def initialize_linked_list_from_array(array)->LinkedListBidirectional:
+    l = LinkedListBidirectional(head_data=array[0])
     for item in array[1:]:
         l.append_node(item)
     return l
