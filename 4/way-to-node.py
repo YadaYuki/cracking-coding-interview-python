84e27eb515599ece05e8f4230a64b4c2ab432087
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-02-09 22:42:08 +0900

    add search by bfs

--- 4/way-to-node.py
+++ 4/way-to-node.py
@@ -19,6 +19,7 @@ def search_way_dfs(directed_graph,start_node=0,end_node=8):
                 all_adjacent_node_visited = False
                 stack.append(adjacent_node)
                 break
+
         if all_adjacent_node_visited == True:
             visited_node = stack.pop()
             is_visited_arr[visited_node] = True
@@ -29,8 +30,23 @@ def search_way_dfs(directed_graph,start_node=0,end_node=8):
     
 
 def search_way_bfs(directed_graph,start_node=0,end_node=8):
-    pass
+    q = queue.Queue()
+    q.put(start_node)
+    is_visited_arr = [False for _ in range(len(directed_graph))]
+    while q.qsize() != 0:
+        current_node = q.get()
+        is_visited_arr[current_node] = True
 
+        adjacent_node_way = directed_graph[current_node]
+
+        if adjacent_node_way[end_node] == WAY:
+            return True
+        
+        for adjacent_node in range(len(adjacent_node_way)):
+            if (adjacent_node_way[adjacent_node] == WAY) and (not is_visited_arr[adjacent_node]):
+                q.put(adjacent_node)
+
+    return False
 
 if __name__ == "__main__":
     directed_graph = [
@@ -45,5 +61,7 @@ if __name__ == "__main__":
         [0,0,0,0,0,0,0,0,0],
     ]
     print(search_way_dfs(directed_graph)) # True
-    print(search_way_dfs(directed_graph,start_node=2,end_node=5)) # True
+    print(search_way_dfs(directed_graph,start_node=1,end_node=4)) # False
+    print(search_way_bfs(directed_graph)) # True
+    print(search_way_bfs(directed_graph,start_node=1,end_node=4)) # False
     
